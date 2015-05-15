1a4e5d319cd1bb714b546a284810f8e838dbef1a
Lexi Mithel <lexi.mithel22@gmail.com>
2015-05-15 15:55:53 -0700

    new createview

--- admin_apps/mileage/urls.py
+++ admin_apps/mileage/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import patterns, url, include, handler404
 from mileage import views
 
 urlpatterns = patterns ('',
-    #url(r'^create/$', views.TripCreate.as_view(), name='create'),
+    url(r'^create/$', views.TripCreate.as_view(), name='create'),
     url(r'^$', views.TripList.as_view(), name='list'),
     #url(r'^d/(?P<pk>\d+)/$', views.TripDetail.as_view(), name='detail'),
     )
--- admin_apps/mileage/views.py
+++ admin_apps/mileage/views.py
@@ -1,8 +1,23 @@
 # mileage/views.py
-
+from django.core.urlresolvers import reverse
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 from mileage.models import Trip
+#from mileage.models import User
 
 
 class TripList(ListView):
     model = Trip
+
+class TripCreate(CreateView):
+	model = Trip
+	fields = ['trip_begin','trip_end','description','paid']
+
+	def form_valid(self, form):
+		self.object = form.save(commit=False)
+		self.object.user = self.request.user
+		self.object.save()
+		return super(TripCreate, self).form_valid(form)
+
+	def get_success_url(self):
+		return reverse('mileage:list')
