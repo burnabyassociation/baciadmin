13f78c024ca25ab914790b3bfa0324022d7035fd
derikn <derikn@gmail.com>
2015-05-27 02:51:01 -0700

    fixed validation for wizard form. moved cleaning to forms from models

--- admin_apps/mileage/forms.py
+++ admin_apps/mileage/forms.py
@@ -1,14 +1,59 @@
 from django import forms 
 from django.db import models
+from django.core.exceptions import ValidationError
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, ButtonHolder
+from crispy_forms.bootstrap import StrictButton
 
 from mileage.models import Trip, Payperiod
 
-class TripStartForm(forms.Form):
-    	trip_begin = forms.CharField(label="Odometer Start")
-    	description = forms.CharField(label="Trip Purpose")
+class TripStartForm(forms.ModelForm):
 
-class TripEndForm(forms.Form):
-    	trip_end = forms.CharField(label="Odometer End")
+    def __init__(self, *args, **kwargs):
+        super(TripStartForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            'trip_begin',
+            'description',
+            ButtonHolder (
+                Submit('next', 'Next', css_class='btn-primary')
+                )
+            )
+
+    def clean(self):
+        super(TripStartForm, self).clean()
+        cleaned_data = self.cleaned_data
+        trip_data = cleaned_data.get('trip_begin')
+        if trip_data < 0:
+            raise ValidationError("Beginning Mileage cannot be negative.")
+        return cleaned_data
+
+    class Meta:
+        model = Trip
+        fields = ['trip_begin', 'description']
+
+class TripEndForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(TripEndForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            'trip_end',
+            ButtonHolder (
+                Submit('add', 'Add', css_class='btn-primary')
+                )
+            )
+    def clean(self):
+        super(TripEndForm, self).clean()
+        cleaned_data = self.cleaned_data
+        trip_data = cleaned_data.get('trip_end')
+        if trip_data < 0:
+            raise ValidationError("Ending Mileage cannot be negative.")
+        return cleaned_data
+
+    class Meta:
+        model = Trip
+        fields = ['trip_end']
 
 class ApproveForm(forms.Form):
     	approved = forms.BooleanField(label="Approved?")
